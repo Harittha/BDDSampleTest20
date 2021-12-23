869786309791bd0c67e6e21e028bf85923fdb9e0
Harittha <96522234+Harittha@users.noreply.github.com>
2021-12-23 16:55:27 +0530

    Update GooglePageTest.java
    Original

--- src/test/java/stepDef/GooglePageTest.java
+++ src/test/java/stepDef/GooglePageTest.java
@@ -40,7 +40,7 @@ public class GooglePageTest {
 		    // Write code here that turns the phrase above into concrete actions
 		 //   throw new io.cucumber.java.PendingException();
 			WebElement searchBox = driver.findElement(By.name("q"));
-			searchBox.sendKeys("Java Tutorial Page");
+			searchBox.sendKeys("Java Tutorial");
 			searchBox.submit();
 		}
 		@Then("Should dislay Java Result Page")
